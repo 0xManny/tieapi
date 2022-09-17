d0c9549f929e4a8142bc5afb3a651cbb036a8131
0xManny <0xmanny.eth@gmail.com>
2022-09-17 03:24:07 -0400

    initial push

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route("/")
+def home():
+    return "API is working fine"
+
+
+if __name__ == "__main__":
+    #app.debug = True
+    app.run(host="0.0.0.0",port= 5000)
