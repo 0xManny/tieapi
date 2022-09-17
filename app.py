dd493ae35ed6866c8ec17e528f56afdf3dffe0c1
0xManny <0xmanny.eth@gmail.com>
2022-09-17 03:40:13 -0400

    test scheduler

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, jsonify
+from flask_apscheduler import APScheduler
 import requests
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+scheduler = APScheduler()
+
+var = 0
 
 @app.route("/")
 def home():
     return {
-        'msg': 'API is working fine'
+        'msg': 'API is working fine',
+        'var': var
     }
+
+def scheduleTask():
+    var += 1
 
 
 if __name__ == "__main__":
     #app.debug = True
+    scheduler.add_job(id = 'Scheduled Task', func=scheduleTask, trigger="interval", seconds=10)
+    scheduler.start()
     app.run(host="0.0.0.0",port= 5000)
